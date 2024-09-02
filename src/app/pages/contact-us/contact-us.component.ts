4d1be6c2671137e75767067de0f698ef257d7312
Chowdhury Mohammad Irfanuddin <mohammad.irfanuddin@technonext.com>
2024-09-03 01:48:40 +0600

    Work in progress

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ContainerComponent, GridModule, NavModule, FormModule, ButtonModule } from '@coreui/angular';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    ContainerComponent,
+    GridModule,
+    NavModule,
+    FormModule,
+    ButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
-
+  public contactForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
+    subject: new FormControl(null, [Validators.required]),
+    message: new FormControl(null, [Validators.required]),
+  })
 }
